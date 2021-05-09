a32b4692e56884e88be9dd46b11e92e4ccbf934d
Prashant-Jhim <pkjhim2001@gmail.com>
2021-05-09 10:10:56 +0530

    update

--- pages/Pictures.js
+++ pages/Pictures.js
@@ -1,7 +1,14 @@
-import Images from '../pages/Pictures/Data'
 import Card from '../pages/Card'
 import styles from '../styles/Pictures/Pictures.module.css'
 let Pictures = () =>{
+    let Images = [
+        {src:'/Simp (2).jpg'},
+        {src:'/Simp (3).jpg'},
+        {src:'/Simp (4).jpg'},
+        {src:'/Simp (5).jpg'},
+        {src:'/Simp (6).jpg'},
+        {src:'/Simp (7).jpg'}
+    ]
     let Print = (arr) =>{
         console.log(arr)
         return(
